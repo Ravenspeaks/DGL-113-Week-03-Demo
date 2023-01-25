cf2c25bd6a37711ff72d84e78351a41d3db03cab
Ravenspeaks <90291456+Ravenspeaks@users.noreply.github.com>
2023-01-25 15:59:37 -0800

    added duration and description functions

---
+++ docs/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function addItem() {
+    let durationElement = document.getElementById( 'duration');
+    let duration = durationElement.value;
+    console.log( duration );
+
+    let descriptionElement = document.getElementById( 'description');
+    let description = descriptionElement.value;
+    console.log( description );
+
+}
