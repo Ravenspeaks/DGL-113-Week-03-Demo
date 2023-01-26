14de6dd0fb5fd4ee06cbf297e8b7650e65648b0f
Ravenspeaks <90291456+Ravenspeaks@users.noreply.github.com>
2023-01-25 16:23:44 -0800

    adding to do

--- docs/app.js
+++ docs/app.js
@@ -1,12 +1,22 @@
 "use strict";
 
+let count = 0;
+let items = [];
+
 function addItem() {
     let durationElement = document.getElementById( 'duration');
     let duration = durationElement.value;
     console.log( duration );
 
-    let descriptionElement = document.getElementById( 'description');
+    let descriptionElement = document.getElementById( 'description' );
     let description = descriptionElement.value;
     console.log( description );
+
+    count += 1;
+
+    items.push( `<tr><td>${count}</td><td>${duration}</td><td>${description}</td></tr>` );
+
+    let todosElement = document.getElementById( 'todos' );
+    todosElement.innerHTML = items.join( '' );
 
 }
